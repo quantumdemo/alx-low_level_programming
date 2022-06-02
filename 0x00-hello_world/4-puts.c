0ea299725c10ab809411a0d013b832994233a614
quantumdemo <olalekanakin2019@gmail.com>
2022-06-02 07:40:59 -0700

    getting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-gcc puts(""Programming is like building a multilingual puzzle");
+#include <stdio.h>
+int main()
+{
+puts(""Programming is like building a multilingual puzzle");
+return(0);
+}
