cab4b77605a6dded1818b7f66327fcb8367ab937
quantumdemo <olalekanakin2019@gmail.com>
2022-06-02 16:58:17 -0700

    finall

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",9);
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 20);
 	return (1);
 }
