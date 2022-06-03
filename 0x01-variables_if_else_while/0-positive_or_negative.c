54c216bed0b3bd9786d7e638884efad460f15a9f
quantumdemo <olalekanakin2019@gmail.com>
2022-06-03 09:15:22 -0700

    add

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,21 +11,21 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("What is the number?\n");
-	scanf("%ld\n");
+	scanf("%d\n");
 	
 	if (n > 0);
 	{
-		printf("is positive\n");
+		printf("is positive %d\n", n);
 	}
 	if (n = 0);
 	{
-		printf("is zero\n");
+		printf("is zero %d\n", n);
 	}
 	else
 	{
 		if (n < 0);
 		{
-			printf("is negative\n");
+			printf("is negative %d\n", n);
 		}	
 	}
 	return (0);
