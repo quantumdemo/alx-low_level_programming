e01735b4a52f38444cdd176ac8da114053f3e9a1
quantumdemo <olalekanakin2019@gmail.com>
2022-06-04 09:00:59 -0700

    correct

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print single numbers, between 1-10
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
