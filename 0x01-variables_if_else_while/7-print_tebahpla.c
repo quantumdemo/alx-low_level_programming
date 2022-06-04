c5585f96cea6e8e723e4ed584a79c59253ee9094
quantumdemo <olalekanakin2019@gmail.com>
2022-06-04 15:21:28 -0700

    done7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints the lowercase alphabet in reverse, followed by a new line.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha <= 'a'; alpha--)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
