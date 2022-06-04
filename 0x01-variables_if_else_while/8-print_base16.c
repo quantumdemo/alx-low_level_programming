75201679c13c2553307f56d78e7613948e8c9006
quantumdemo <olalekanakin2019@gmail.com>
2022-06-04 16:46:05 -0700

    done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	char num;
-	char lsd:
+	int num;
+	char lsd;
 		
-	for (num = '0'; num <= '9'; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 16) + '0');
 	}
