19fa2b42fcfa47ab31c913ae5ec2b6275d6a000d
quantumdemo <olalekanakin2019@gmail.com>
2022-06-05 05:54:36 -0700

    added

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints all possible combinations of single
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+	putchar(',');
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
