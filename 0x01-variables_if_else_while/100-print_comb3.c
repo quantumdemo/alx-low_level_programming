437a8cd8092ee92ad787396bae5a4d9a06542f0b
quantumdemo <olalekanakin2019@gmail.com>
2022-06-05 07:53:28 -0700

    finish

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,16 +13,16 @@ int main(void)
 
 	for (a = 0; a <= 8; a++)
 	{
-		for (b = a + 1; b <=9; b++)
+		for (b = a + 1; b <= 9; b++)
 		{
 			putchar((a % 10) + '0');
 			putchar((b % 10) + '0');
-			if (a ==8 && b == 9)
+			if (a == 8 && b == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
 		}
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
