0c9b6fc86854e2d6339ddf6314b926390bf29f30
quantumdemo <olalekanakin2019@gmail.com>
2022-06-06 04:54:34 -0700

    done2

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 8; a++)
+	{
+		for (b = a +1; b <= 9; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 8 && b == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
