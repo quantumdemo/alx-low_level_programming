9ebfbd696bac4c206d2ec3f615304f3909b2e292
quantumdemo <olalekanakin2019@gmail.com>
2022-06-06 04:22:54 -0700

    done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints all possible different combinations
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c;
+	for (a = 0; a <= 7; a++)
+	{
+		for (b = a + 1; b <= 8; b++)
+		{
+			for (c = b + 1; c <= 9; c++)
+			{
+				putchar((a % 10) + '0');
+				putchar((b % 10) + '0');
+				putchar((c % 10) + '0');
+				
+				if (a == 7 && b == 8 && c == 9)
+					continue;
+					putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
