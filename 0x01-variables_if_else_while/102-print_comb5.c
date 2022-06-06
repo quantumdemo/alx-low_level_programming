daeb0a41ef59054beeeb549e971ae7bd36e6d8ce
quantumdemo <olalekanakin2019@gmail.com>
2022-06-06 05:07:09 -0700

    getting it

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,16 +11,16 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 8; a++)
+	for (a = 0; a <= 98; a++)
 	{
-		for (b = a +1; b <= 9; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
 			putchar((a / 10) + '0');
 			putchar((a % 10) + '0');
 			putchar(' ');
 			putchar((b / 10) + '0');
 			putchar((b % 10) + '0');
-			if (a == 8 && b == 9)
+			if (a == 98 && b == 99)
 				continue;
 			putchar(',');
 			putchar(' ');
