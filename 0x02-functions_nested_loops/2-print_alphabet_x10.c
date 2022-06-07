468ed3fb6d672aa1eec04dff6e3e3ad512a13fda
quantumdemo <olalekanakin2019@gmail.com>
2022-06-07 09:51:03 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - alphabet
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	a--;
+	_putchar(10);
+	alpha = 'a';
+}
