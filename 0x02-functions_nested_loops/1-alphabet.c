5f025a6efd412e28707fd8ec283f2802a4b6ed8a
quantumdemo <olalekanakin2019@gmail.com>
2022-06-07 09:25:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - alphabeth
+ */
+void print_alphabet(void)
+{
+	char alpha;
+	
+	alpha = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(alpha);
+		a++;
+	}
+
+	_putchar('\n');
+}
