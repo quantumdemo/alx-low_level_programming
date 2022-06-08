e8a69c9c39f02f7878c5c8e7af59d625d479722d
quantumdemo <olalekanakin2019@gmail.com>
2022-06-08 04:39:29 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number.
+ * @n: an input sign
+ * Description: Returns 1 and prints + if n is greater than
+ * Return: 1 or 0 in otherwise.
+ */
+int print_sign(int n)
+{
+	int operand;
+
+	for (operand = 0)
+	{
+		if (n == operand)
+			return (0);
+	}
+	for (operand > 0)
+	{
+		if (n == operand)
+			return (1);
+	}
+	for (operand < 0)
+	{
+		if (n == operand)
+			return (-1);
+	}
+}
+	
