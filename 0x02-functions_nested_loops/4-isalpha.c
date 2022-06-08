a07b76b6cca25a9a3de3cdfc54cd5a534c01156a
quantumdemo <olalekanakin2019@gmail.com>
2022-06-08 03:48:00 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - lowercase or uppercase
+ *
+ * Description: function returns 1 if the character
+ * Return: 1 or 0 in otherwise.
+ */
+int _isalpha(int c)
+{
+	char n1;
+	char n2;
+
+	for (n1 = 'a'; n1 <= 'z'; n1++)
+	{
+		for (n2 = 'A'; n2 <= 'Z'; n2++)
+		{
+			if (n1 == c || n2 == c)
+				return (0);
+		}
+	}
+	return (1);
+}
