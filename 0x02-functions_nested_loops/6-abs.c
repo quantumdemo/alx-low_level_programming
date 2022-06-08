4df36db3842fc9870b1a75986acaa388fae86be6
quantumdemo <olalekanakin2019@gmail.com>
2022-06-08 05:20:27 -0700

    done

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * _abs - computes the absolute value of an integer.
- *  @n: An input number
+ *  @a: An input number
  *  Description: nothing
+ *  Return: Absolut value of number a
  */
 int _abs(int a)
 {
-	if(a >= 0)
+	if (a >= 0)
 		return (a);
 	else
 		return (a * -1);
-}		
+}	
