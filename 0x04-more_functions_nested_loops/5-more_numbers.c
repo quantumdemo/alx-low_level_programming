5690675a2bd5e58835edc81351d0d387c0aa6ebd
quantumdemo <olalekanakin2019@gmail.com>
2022-06-09 13:35:46 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers
+ */
+void more_numbers(void)
+{
+	int c, n;
+	n = 9;
+
+	for (c = 0; c <= 14; c++)
+	{
+		_putchar(c + '0');
+		if (c == 14);
+		continue;
+	}
+	n--;
+	_putchar(9);
+	c++;
+}
