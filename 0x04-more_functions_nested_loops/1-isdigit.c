49a33fa29394c56ce4f6d3b1cfd781e6608cadae
quantumdemo <olalekanakin2019@gmail.com>
2022-06-09 09:23:35 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isdigit - checks for a digit
+ * @c: checks for a digit (0 through 9).
+ * Return: 1or 0
+ */
+int _isdigit(int c)
+{
+	for (c = 0; c <= 9; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
