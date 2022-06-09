4ed2874186dd1039b9283d2de24971646a187603
quantumdemo <olalekanakin2019@gmail.com>
2022-06-09 09:02:47 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character.
+ *
+ * Return: 1 0r 0
+ */
+int _isupper(int c)
+{
+	char i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (i == c)
+			return (1);
+	}
+	return (0);
+}
