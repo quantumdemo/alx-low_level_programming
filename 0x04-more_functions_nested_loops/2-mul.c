9434b61a56e173e004c77d9f344f2717a3b0da69
quantumdemo <olalekanakin2019@gmail.com>
2022-06-09 10:22:24 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - multiplies two integers.
+ * @a - integer
+ * @b - integer
+ */
+int mul(int a, int b)
+{
+	int n;
+
+	if (a >= -402 && a <= 98)
+	{
+		if (b >= 409 && <= 1024)
+		{
+			n = a * b;
+			_putchar(n);
+		}
+	}
+}
