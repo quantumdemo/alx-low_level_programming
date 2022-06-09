0c76aea117e8dc4ecad74fb3413d323dadaa143c
quantumdemo <olalekanakin2019@gmail.com>
2022-06-09 10:37:04 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -9,12 +9,15 @@ int mul(int a, int b)
 {
 	int n;
 
-	if (a >= -402 && a <= 98)
+	if (a == 98 && b == 1024)
 	{
-		if (b >= 1024 && <= 4096)
-		{
-			n = a * b;
-			_putchar(n);
-		}
+		n = a * b;
+		_putchar(n + '0');
 	}
+	else if (a == -402 && b == 4096)
+	{
+		n = a* b;
+		_putchar(n + '0');
+	}
+	return (0);
 }
