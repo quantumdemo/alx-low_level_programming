53577c051bca94681953c9a902ec98fac15fd58f
quantumdemo <olalekanakin2019@gmail.com>
2022-06-09 12:46:34 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putxhar(a + '0');
+	}
+	return (0);
+}
