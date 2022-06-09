3050e5504f910204179c8f112759465925850cc3
quantumdemo <olalekanakin2019@gmail.com>
2022-06-09 09:52:03 -0700

    finish

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,19 +2,15 @@
 #include "main.h"
 /**
  * _isdigit - checks for a digit
- *
- * @c: the character to evaluate
- * 
+ * @c: the character to evaluate 
  * Return: get 1 if c is digit 
  * get 0 otherwise
  */
 int _isdigit(int c)
 {
-	int n
 
-	for (n = 0; n <= 9; n++)
+	for (c = 0; c <= 9; c++)
 	{
-		if (n == c)
 		return (1);
 	}
 	return (0);
