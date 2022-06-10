9ba19be3e4a6691000a1214e67641e4515953482
quantumdemo <olalekanakin2019@gmail.com>
2022-06-10 07:45:17 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,10 +8,13 @@ void print_diagonal(int n)
 	for (n = 1; n <= 10; n++)
 	{
 		_putchar(2);
+		_putchar(4);
+		_putchar(5);
 		_putchar(10);
 	}
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
+	_putchar('\n');
 }
