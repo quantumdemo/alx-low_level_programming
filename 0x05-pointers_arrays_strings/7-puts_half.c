ee295b40e83d333804e07436952a9bf092419c55
quantumdemo <olalekanakin2019@gmail.com>
2022-06-13 08:07:18 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,10 +7,11 @@ void puts_half(char *str);
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++){
 		for (i /= 2; str[i] != '\0'; i++)
 		{
 			_putchar(str[i]);
 		}
+	}
 	_putchar('\n');
 }
