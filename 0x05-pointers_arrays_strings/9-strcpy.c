b46a0678c91dadb13a3be45a1f167664c200d887
quantumdemo <olalekanakin2019@gmail.com>
2022-06-13 08:52:31 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  * *_strcpy -  copies the string pointed to by src
  * @dest: char type string
  * @src: char type string
@@ -13,7 +12,7 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = -1;
 
-	do{
+	do {
 		i++;
 		dest[i] = src[i];
 	} while (src[i] != '\0');
