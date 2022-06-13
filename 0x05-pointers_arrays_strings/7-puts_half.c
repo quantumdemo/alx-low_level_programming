b2d3c1f3acb3d51aed1b5bd5e8ff7ce9442cc056
quantumdemo <olalekanakin2019@gmail.com>
2022-06-13 09:09:34 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,16 +2,18 @@
 /**
  * puts2 - that prints every other character of a string, starti
  * @str: the used string reference
+ * Return: 0
  */
 void puts_half(char *str);
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++){
-		for (i /= 2; str[i] != '\0'; i++)
-		{
-			_putchar(str[i]);
-		}
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
