5dfd50dd909b32fd3c10ba09aba4818798a188ca
quantumdemo <olalekanakin2019@gmail.com>
2022-06-13 07:48:53 -0700

    sdf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - that prints every other character of a string, starti
+ * @str: the used string reference
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		for (i /= 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	_putchar('\n');
+}
