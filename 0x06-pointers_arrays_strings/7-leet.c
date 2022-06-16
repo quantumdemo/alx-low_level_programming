38a196d96ccddc5facf39a50f662152c65bd3485
quantumdemo <olalekanakin2019@gmail.com>
2022-06-16 14:57:31 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,22 +8,19 @@
  */
 char *leet(char *s)
 {
-	int uc;
-	int x = 0, y, uc = 0;
-	int lc[5] = {97, 101, 111, 116, 108};
-	int code[5] = {4, 3, 0, 7, 1};
+	int indx1 = 0, indx2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (*(s + x))
+	while (str[indx1])
 	{
-		for (y = 0; y < 5; y++)
+		for (indx2 = 0; indx2 <= 7; indx2++)
 		{
-			uc = lc[y] - 32;
-			if (s[x] == lc[y] || s[x] == uc)
-			{
-				s[x] = code[y] + 48;
-			}
+			if (str[indx1] == leet[indx2] || str[indx1] - 32 == leet[indx2])
+				str[indx1] = indx2 + '0';
 		}
-		x++;
+
+		indx1++;
 	}
-	return (s);
+
+	return (str);
 }
