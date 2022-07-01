2cb3dafb323599591f599cf889aac96a994620a2
Alimi Olalekan <105062689+quantumdemo@users.noreply.github.com>
2022-07-01 02:39:05 +0100

    Update main.c

--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -13,4 +13,28 @@ int main(void)
 	concat = string_nconcat("Best ", "School !!!", 6);
 	printf("%s\n", concat);
 	free(concat);
+        
+        concat = string_nconcat("Holberton ", "School", 4);
+	printf("%s\n", concat);
+	free(concat);
 
+	concat = string_nconcat("", "Hello", 12);
+	printf("%s\n", concat);
+	free(concat);
+
+	concat = string_nconcat("Hello", "", 12);
+	printf("%s\n", concat);
+	free(concat);
+
+	concat = string_nconcat("Hello", NULL, 12);
+	printf("%s\n", concat);
+	free(concat);
+	concat = string_nconcat(NULL, "Hello", 0);
+	printf("%s\n", concat);
+	free(concat);
+	concat = string_nconcat(NULL, NULL, 10);
+	printf("%s\n", concat);
+	free(concat);
+
+	return (0);
+}
