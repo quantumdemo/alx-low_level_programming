db67593687966a3808ba58af23780f83f49c6c73
quantumdemo <olalekanakin2019@gmail.com>
2022-07-22 04:22:13 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,22 +9,15 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	uint a = 0;
 
-	if (b == '\0')
+	if (!b)
 		return (0);
-
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	while (*b)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (*b != '1' && *b != '0')
 			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		a = a * 2 + (*b++ - 48);
 	}
-	return (num);
+	return (a);
 }
